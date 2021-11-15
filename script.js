e0499f9ed04eea1dbf4b779a71bc58cf430397e7
Fredrik <faltofredrik@gmail.com>
2021-11-15 08:24:30 +0100

    Added some images and modified html + css pages

---
+++ script.js
@@ -0,0 +1,9 @@
+function august() {
+    location.href = 'august.html';
+}
+
+
+// location.href = 'august.html'
+// document.getElementById("buttonAugust").onclick = function() {
+//     location.href = 'august.html';
+// }
